96aff8defaef1a38975f0fc18542e14ea8706052
martinmastronardi <mastronardi390@gmail.com>
2022-08-30 22:05:31 -0300

    Listar agregar

--- ProyectoFinal/backend/routes/admin/novedades.js
+++ ProyectoFinal/backend/routes/admin/novedades.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
     layout: 'admin/layout',
-    persona: req.session.nombre
+    persona: req.session.nombre,
+    novedades
   });
 });
 
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.novedad != "") {
+      await novedadesModel.insertNovedades(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,
+        message: 'Debe completar todos los campos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se pudo cargar la novedad'
+    });
+  }
+});
 
 module.exports = router;

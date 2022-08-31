98464c6bc55cb9a9750e503d6b37f1c22b7308b5
martinmastronardi <mastronardi390@gmail.com>
2022-08-31 20:11:38 -0300

    eliminar y modificar

--- ProyectoFinal/backend/models/novedadesModel.js
+++ ProyectoFinal/backend/models/novedadesModel.js
@@ -17,4 +17,26 @@ async function insertNovedades(obj) {
     }
 }
 
-module.exports = { getNovedades, insertNovedades }
+async function borrarNovedadPorId(id) {
+    var query = 'delete from novedades where id = ? ';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+async function getNovedadesById(id) {
+    var query = 'select * from novedades where id = ? ';
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadPorId(obj, id) {
+    try {
+        var query = 'update novedades set ? where id = ? ';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = { getNovedades, insertNovedades, borrarNovedadPorId, getNovedadesById, modificarNovedadPorId }
--- ProyectoFinal/backend/routes/admin/novedades.js
+++ ProyectoFinal/backend/routes/admin/novedades.js
@@ -40,4 +40,39 @@ router.post('/agregar', async (req, res, next) => {
   }
 });
 
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.borrarNovedadPorId(id);
+  res.redirect('/admin/novedades')
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadesById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      novedad: req.body.novedad
+    }
+
+    await novedadesModel.modificarNovedadPorId(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  }
+  catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modifico la novedad'
+    })
+  }
+})
+
 module.exports = router;

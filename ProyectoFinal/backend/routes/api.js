410862cfff08c3c915a7640776e03fdad86a99b1
martinmastronardi <mastronardi390@gmail.com>
2022-09-07 19:07:33 -0300

    mail

--- ProyectoFinal/backend/routes/api.js
+++ ProyectoFinal/backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
@@ -25,4 +26,29 @@ router.get('/novedades', async function (req, res, next) {
     });
     res.json(novedades);
 });
+
+router.post('contacto', async (req, res) => {
+    const mail = {
+        to: 'mastronardi390@gmail.com',
+        subject: 'Contacto Web',
+        html: `${req.body.nombre} se contacto a traves de la web y 
+        quiere mas informacion a este correo: 
+        ${req.body.email}<br>Su tel es:
+        ${req.body.telefono}`
+    }
+    const transport=nodemailer.createTransport({
+        host:process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+    await transport.sendMail(mail)
+    req.statusCode(201).json({
+        error: false,
+        message: 'Mensaje Enviado'
+    }); 
+});
+
 module.exports = router

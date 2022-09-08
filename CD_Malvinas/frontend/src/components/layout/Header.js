e9e3cebb781b5c517e95a6e0b59cc29502013226
martinmastronardi <mastronardi390@gmail.com>
2022-09-08 16:55:56 -0300

    cd

---
+++ CD_Malvinas/frontend/src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import '../../style/Layout.css'
+
+const Header = (props) => {
+    return (
+        <div>
+            <header>
+                <div class="holder">
+                    <img className='ImgHeader' src='img/Carrefour_viejo.png' width="200" alt='Registro de equipos'/>
+                   
+                </div>
+            </header>
+        </div>
+    );
+}
+export default Header;
---
+++ CD_Malvinas/frontend/src/pages/HomePage.js
@@ -0,0 +1,18 @@
+const HomePage = (props) => {
+    return ( 
+        
+    <main className="holder">
+   
+    <div className="ColBien" >
+        <div className="Bienvenidos">
+            <h2>Bienvenidos</h2>
+       
+        </div>
+    </div>
+
+
+
+</main>
+    );
+}
+export default HomePage;
